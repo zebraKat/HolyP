ba3ac8e12257a4e95318d928dc8184bccc674ae2
zebrakat <zebrakat@noreply.codeberg.org>
2026-02-10 20:26:44 -0500

    lexer: begin working on the lexer

--- src/holyp-lex.lua
+++ src/holyp-lex.lua
@@ -1,14 +1,14 @@
 --[[
--	If you are using this on PolyToria,
--	you may need to change these imports to fit PolyToria's style.
+--	If you are using this on PolyToria,
+--	you may need to change these imports to fit PolyToria's style.
 --]]
 local Position = require("src/util/Position")
 local Diagnostic = require("src/util/Diagnostic")
 local Token = require("src/holyp-token")
 
 --[[
--	This is the lexer for HolyP.
--	The lexer splits the source code into tokens that the parser can go through.
+--	This is the lexer for HolyP.
+--	The lexer splits the source code into tokens that the parser can go through.
 --]]
 local Lexer = {}
 
@@ -18,6 +18,21 @@ local Lexer = {}
 --	Note that this expects the diagnostics list length to be 0.
 --]]
 function Lexer.New(source, diagnostics)
+	if type(diagnostics) ~= "table" or #diagnostics ~= 0 then
+		error("Expected diagnostics to be a table of length 0.")
+	end
+
+	if type(source) ~= "string" then
+		table.insert(
+			diagnostics,
+			Diagnostic.New(
+				"Expected sourcecode to be a string, instead found "..type(source),
+				Diagnostic.Mode.Error,
+				Position.New()
+			)
+		)
+	end
+
 	local self = {
 		Index = 1,
 		Source = source,
@@ -26,23 +41,49 @@ function Lexer.New(source, diagnostics)
 	}
 	
 	--[[
-	-	Returns the character at the current index, then increases the index by one.
-	-	Note that if the index is beyond the length of the string, then it will return "EOF".
+	--	Returns the character at the current index, then increases the index by one.
+	--	Note that if the index is beyond the length of the string, then it will return "EOF".
 	--]]
-	function self.Eat()
+	local function Eat()
 		local c = self.At()
 		self.Index = self.Index + 1
+		if c == '\n' then self.Position.y = self.Position.y + 1; self.Position.x = 1; end
 		return c
 	end
 
 	--[[
-	-	Returns the character at the current index.
-	-	Note that if the index is beyond the length of the string, then it will return "EOF".
+	--	Returns the character at the current index.
+	--	Note that if the index is beyond the length of the string, then it will return "EOF".
 	--]]
-	function self.At()
+	local function At()
 		if #self.Source < self.Index then return "EOF" end
 		return string.sub(self.Source, self.Index, self.Index)
 	end
+
+	--[[
+	--	Converts continuous numbers into a token.
+	--	Keep in mind that it will handle floats.
+	--]]
+	local function TokenizeNumber()
+		local NumberString = Eat()
+		
+	end
+
+	local function TokenizeString()
+		local String = Eat()
+		
+	end
+
+	local function TokenizeChar()
+		_ = Eat()
+		local Char = Eat()
+		if Eat() ~= "'" then
+			--[[ TODO: ERROR ]]--
+		end
+		
+	end
+
+	function self.
 
 end
 
